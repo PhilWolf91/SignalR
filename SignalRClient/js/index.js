873e2c1e368e4438959946844db4a0dcc698f4fe
Phillip Wolf <Phillip.wolf91@gmail.com>
2017-05-06 23:09:11 -0400

    Init

---
+++ SignalRClient/js/index.js
@@ -0,0 +1,57 @@
+﻿window.appConfig = {
+    'serverUrl': "http://localhost:58481"
+}
+
+$(function () {
+    if ($.signalR) {
+
+    }
+    
+    var username = prompt("username");
+    var success = `
+                <div class ="serverMessage">
+                    <span>--- WELCOME, ${username} ---</span>
+                </div>
+            `
+    $('#chatbox').append(success)
+
+
+    $.connection.hub.logging = true
+    $.connection.hub.qs = { 'username': username }
+    $.connection.hub.url = window.appConfig.serverUrl + "/signalr";
+    var chatHub = $.connection.chatHub
+
+    chatHub.client.broadcastMessage = function (name, message) {
+        console.log("Data Incoming ---- Name: " + name + " Message: " + message);
+        var message = `
+            <div class="row">
+                <span class="col-sm-2">${name}:</span> 
+                <span class="col-sm-9"> ${message}</span>
+            </div>
+        `
+        $('#chatbox').append(message)
+    }
+
+
+    $('#messageButton').on('click', function () {
+        chatHub.server.send(username, $('#messageBox').val())
+    })
+    $('#messageForm').on('submit', function (e) {
+        e.preventDefault();
+        chatHub.server.send(username, $('#messageBox').val())
+    })
+
+    $.connection.hub.start()
+        .done(function () {
+            console.log("Connected successfully to SignalR");
+            var success = `
+                <div class ="serverMessage">
+                    <span>--- Successfully connected to the server ---</span>
+                </div>
+            `
+            $('#chatbox').append(success)
+        })
+        .fail(function () {
+            console.log("Unable to connect to SignalR")
+        })
+})
---
+++ SignalRServer/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+
+[assembly: OwinStartup(typeof(SignalRServer.Startup))]
+namespace SignalRServer
+{
+    
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Map("/signalr", map =>
+            {
+                //map.UseCors(CorsOptions.AllowAll);
+                var hubConfiguration = new HubConfiguration { EnableJSONP = true };
+                map.RunSignalR(hubConfiguration);
+            });
+        }
+
+    }
+}
