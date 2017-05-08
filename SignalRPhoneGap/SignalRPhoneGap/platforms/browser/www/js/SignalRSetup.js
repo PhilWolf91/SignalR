de19458d739a17fbe1bd2404a867cdd8cc68d642
Phil Wolf <Phillip.wolf91@gmail.com>
2017-05-08 11:11:59 -0400

    Tried Enabling JSONP on connection

--- SignalRPhoneGap/SignalRPhoneGap/platforms/browser/www/js/SignalRSetup.js
+++ SignalRPhoneGap/SignalRPhoneGap/platforms/browser/www/js/SignalRSetup.js
@@ -1,115 +1,61 @@
-var appConfig = {
-    'serverUrl': "http://wolf-signalrserver.azurewebsites.net"
-}
-
-var initiateSignalR = function(){
-    if ($.signalR) {
-
+    var appConfig = {
+        'serverUrl': "http://wolf-signalrserver.azurewebsites.net"
     }
-    
-    var username = prompt("username");
-    var success = `
-                <div class ="serverMessage">
-                    <span>--- WELCOME, ${username} ---</span>
-                </div>
-            `
-    $('#chatbox').append(success)
 
+    var initiateSignalR = function(){
+        if ($.signalR) {
 
-
-
- $.connection.hub.logging = true
-        $.connection.hub.qs = { 'username': username }
-        $.connection.hub.url = appConfig.serverUrl + "/signalr";
-        var chatHub = $.connection.chatHub
-
-        chatHub.client.broadcastMessage = function (name, message) {
-            console.log("Data Incoming ---- Name: " + name + " Message: " + message);
-            var message = `
-                <div class="row">
-                    <span class="col-sm-2">${name}:</span> 
-                    <span class="col-sm-9"> ${message}</span>
-                </div>
-            `
-            $('#chatbox').append(message)
         }
-
-
-        $('#messageButton').on('click', function () {
-            chatHub.server.send(username, $('#messageBox').val())
-        })
-        $('#messageForm').on('submit', function (e) {
-            e.preventDefault();
-            chatHub.server.send(username, $('#messageBox').val())
-        })
-
-        $.connection.hub.start({ withCredentials: false })
-            .done(function () {
-                console.log("Connected successfully to SignalR");
-                var success = `
+        
+        var username = prompt("username");
+        var success = `
                     <div class ="serverMessage">
-                        <span>--- Successfully connected to the server ---</span>
+                        <span>--- WELCOME, ${username} ---</span>
                     </div>
                 `
-                $('#chatbox').append(success)
+        $('#chatbox').append(success)
+
+
+
+
+    $.connection.hub.logging = true
+            $.connection.hub.qs = { 'username': username }
+            $.connection.hub.url = appConfig.serverUrl + "/signalr";
+            var chatHub = $.connection.chatHub
+
+            chatHub.client.broadcastMessage = function (name, message) {
+                console.log("Data Incoming ---- Name: " + name + " Message: " + message);
+                var message = `
+                    <div class="row">
+                        <span class="col-sm-2">${name}:</span> 
+                        <span class="col-sm-9"> ${message}</span>
+                    </div>
+                `
+                $('#chatbox').append(message)
+            }
+
+
+            $('#messageButton').on('click', function () {
+                chatHub.server.send(username, $('#messageBox').val())
             })
-            .fail(function (err) {
-                console.log("Unable to connect to SignalR")
-                alert(err);
+            $('#messageForm').on('submit', function (e) {
+                e.preventDefault();
+                chatHub.server.send(username, $('#messageBox').val())
             })
 
-
-
-
-
-
-
-
-    // $.getScript("http://wolf-signalrserver.azurwebsites.net/signalr/hubs", function(){
-    //     alert("Loaded HUBS successfully and beggining connection");
-    //     $.connection.hub.logging = true
-    //     $.connection.hub.qs = { 'username': username }
-    //     $.connection.hub.url = appConfig.serverUrl + "/signalr";
-    //     var chatHub = $.connection.chatHub
-
-    //     chatHub.client.broadcastMessage = function (name, message) {
-    //         console.log("Data Incoming ---- Name: " + name + " Message: " + message);
-    //         var message = `
-    //             <div class="row">
-    //                 <span class="col-sm-2">${name}:</span> 
-    //                 <span class="col-sm-9"> ${message}</span>
-    //             </div>
-    //         `
-    //         $('#chatbox').append(message)
-    //     }
-
-
-    //     $('#messageButton').on('click', function () {
-    //         chatHub.server.send(username, $('#messageBox').val())
-    //     })
-    //     $('#messageForm').on('submit', function (e) {
-    //         e.preventDefault();
-    //         chatHub.server.send(username, $('#messageBox').val())
-    //     })
-
-    //     $.connection.hub.start({ withCredentials: false })
-    //         .done(function () {
-    //             console.log("Connected successfully to SignalR");
-    //             var success = `
-    //                 <div class ="serverMessage">
-    //                     <span>--- Successfully connected to the server ---</span>
-    //                 </div>
-    //             `
-    //             $('#chatbox').append(success)
-    //         })
-    //         .fail(function (err) {
-    //             console.log("Unable to connect to SignalR")
-    //             alert(err);
-    //         })
-
-    // }).fail(function(){
-    //     alert("failed to load your HUBS script");
-    // })
+            $.connection.hub.start({ withCredentials: false, jsonp: true })
+                .done(function () {
+                    console.log("Connected successfully to SignalR");
+                    var success = `
+                        <div class ="serverMessage">
+                            <span>--- Successfully connected to the server ---</span>
+                        </div>
+                    `
+                    $('#chatbox').append(success)
+                })
+                .fail(function (err) {
+                    console.log("Unable to connect to SignalR")
+                    alert(err);
+                })
+    }
         
-}
-    
